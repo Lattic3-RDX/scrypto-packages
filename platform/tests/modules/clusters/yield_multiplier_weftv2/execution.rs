990c5802959b84e0512e53063cde10bf7fc6be14
Tymur-Tykva <tykva.tymur@gmail.com>
2025-03-13 13:01:17 +0100

    Created test for execution service

--- platform/tests/modules/clusters/yield_multiplier_weftv2/execution.rs
+++ platform/tests/modules/clusters/yield_multiplier_weftv2/execution.rs
@@ -1,3 +1,4 @@
+use crate::helpers::clusters::yield_multiplier_weftv2::YMWeftV2ClusterService;
 use crate::helpers::prelude::*;
 use crate::helpers::{clusters::yield_multiplier_weftv2::YMWeftV2ClusterFactory, integrations::weftv2::MockWeftV2};
 use indexmap::indexmap;
@@ -469,3 +470,93 @@ fn test_invalid_execute_without_link() {
 }
 
 /* ------------ Operating Services ------------ */
+#[test]
+#[should_panic]
+fn test_invalid_execute_empty_when_service_disabled() {
+    //] Arrange
+    // Create a test runner and platform
+    let (mut runner, platform) = Runner::new_base();
+    let owner_account = runner.owner_account;
+    let alice_account = runner.alice_account;
+
+    // Instantiate a YieldMultiplierWeftCluster
+    let mut weftv2 = MockWeftV2::new(&mut runner);
+    let ym_weftv2_cluster_factory = YMWeftV2ClusterFactory::new(&mut runner.ledger);
+
+    let owner_rule = rule!(require(platform.owner_badge));
+    let supply = runner.faucet.usdt.address;
+    let debt = runner.faucet.xwbtc.address;
+
+    let cluster = ym_weftv2_cluster_factory.instantiate(
+        &mut runner,
+        owner_rule,
+        platform.component,
+        platform.link_badge,
+        platform.user_badge,
+        supply,
+        debt,
+        weftv2.cdp,
+    );
+
+    // Link cluster to platform
+    platform.link(&mut runner, &owner_account, cluster.component);
+
+    // Get a user badge
+    platform.new_user(&mut runner, &alice_account);
+
+    // Get an empty CDP
+    let cdp_id = weftv2.mint_empty(&mut runner, alice_account);
+
+    //] Act & Assert
+    // Open an account
+    let manifest = ManifestBuilder::new()
+        .lock_fee_from_faucet()
+        .create_proof_from_account_of_non_fungibles(alice_account.address, platform.user_badge, vec![NonFungibleLocalId::Integer(0.into())])
+        .pop_from_auth_zone("user_badge")
+        .withdraw_non_fungibles_from_account(alice_account.address, weftv2.cdp, vec![cdp_id.clone()])
+        .take_all_from_worktop(weftv2.cdp, "cdp_bucket")
+        .call_method_with_name_lookup(cluster.component, "open_account", |lookup| {
+            (lookup.proof("user_badge"), lookup.bucket("cdp_bucket"))
+        });
+
+    let receipt = runner.exec("open_account", manifest, &alice_account, Some("clusters/yield_multiplier_weftv2"));
+    receipt.expect_commit_success();
+
+    // Disable the Execute service
+    let manifest = ManifestBuilder::new()
+        .lock_fee_from_faucet()
+        .create_proof_from_account_of_amount(owner_account.address, platform.owner_badge, dec!(1))
+        .call_method(
+            cluster.component,
+            "update_service",
+            manifest_args!(YMWeftV2ClusterService::Execute, false,),
+        );
+    let receipt = runner.exec(
+        "update_cluster_service",
+        manifest,
+        &owner_account,
+        Some("clusters/yield_multiplier_weftv2"),
+    );
+
+    receipt.expect_commit_success();
+
+    // Execute a transaction
+    let manifest = ManifestBuilder::new()
+        .lock_fee_from_faucet()
+        .create_proof_from_account_of_non_fungibles(alice_account.address, platform.user_badge, vec![NonFungibleLocalId::Integer(0.into())])
+        .pop_from_auth_zone("user_badge")
+        .clone_proof("user_badge", "user_badge_2")
+        .call_method_with_name_lookup(cluster.component, "start_execution", |lookup| (lookup.proof("user_badge"),))
+        .take_all_from_worktop(cluster.execution_terms, "execution_term_bucket")
+        .take_all_from_worktop(weftv2.cdp, "cdp_bucket")
+        .call_method_with_name_lookup(cluster.component, "end_execution", |lookup| {
+            (
+                lookup.proof("user_badge_2"),
+                lookup.bucket("cdp_bucket"),
+                lookup.bucket("execution_term_bucket"),
+            )
+        });
+
+    let receipt = runner.exec_and_dump("execute", manifest, &alice_account, Some("clusters/yield_multiplier_weftv2"));
+    receipt.expect_commit_success();
+}
